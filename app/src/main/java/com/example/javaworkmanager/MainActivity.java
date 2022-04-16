cc061d0937e12a5a35d8274e769834ce31254553
abdulhemit <93004625+abdulhemit@users.noreply.github.com>
2022-04-16 13:58:55 +0300

    java workmanager

---
+++ app/src/main/java/com/example/javaworkmanager/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.javaworkmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.work.Constraints;
+import androidx.work.Data;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
+
+import android.os.Bundle;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Data data = new Data.Builder().putInt("intKey",1).build();
+
+        Constraints constraints = new Constraints.Builder()
+                .setRequiresCharging(false)
+                //.setRequiredNetworkType(NetworkType.CONNECTED)
+                .build();
+
+
+        /*
+                WorkRequest workRequest = new OneTimeWorkRequest.Builder(RefreshDataBase.class)
+                .setInputData(data)
+                .setConstraints(constraints)
+                //.setInitialDelay(5, TimeUnit.HOURS)
+                //.addTag("myTag")
+                .build();
+
+        WorkManager.getInstance(this).enqueue(workRequest);
+
+         */
+
+        WorkRequest workRequest = new PeriodicWorkRequest.Builder(RefreshDataBase.class,15, TimeUnit.MINUTES)
+                .setConstraints(constraints)
+                .setInputData(data)
+                .build();
+
+        WorkManager.getInstance(this).enqueue(workRequest);
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(workRequest.getId()).observe(this, new Observer<WorkInfo>() {
+            @Override
+            public void onChanged(WorkInfo workInfo) {
+                if(workInfo.getState() == WorkInfo.State.RUNNING){
+                    System.out.println("Running");
+                }else if(workInfo.getState() == WorkInfo.State.SUCCEEDED){
+                    System.out.println("Succeeded");
+                }else if (workInfo.getState() == WorkInfo.State.FAILED){
+                    System.out.println("Failed");
+                }
+            }
+        });
+
+        //WorkManager.getInstance(this).cancelAllWork();
+
+        /*
+        //Chaining
+        OneTimeWorkRequest oneTimeWorkRequest = new OneTimeWorkRequest.Builder(RefreshDataBase.class)
+                .setInputData(data)
+                .setConstraints(constraints)
+                .build();
+
+        WorkManager.getInstance(this).beginWith(oneTimeWorkRequest)
+                .then(oneTimeWorkRequest)
+                .then(oneTimeWorkRequest)
+                .enqueue();
+
+         */
+    }
+}
---
+++ app/src/main/java/com/example/javaworkmanager/RefreshDataBase.java
@@ -0,0 +1,35 @@
+package com.example.javaworkmanager;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.annotation.NonNull;
+import androidx.work.Data;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class RefreshDataBase extends Worker {
+
+
+    Context myContext;
+    public RefreshDataBase(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+        this.myContext = context;
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        Data data = getInputData();
+        int myNumber = data.getInt("intKey",0);
+        refreshDatabase(myNumber);
+        return Result.success();
+    }
+    private void refreshDatabase(int myNumber){
+        SharedPreferences sharedPreferences = myContext.getSharedPreferences("com.example.javaworkmanager",Context.MODE_PRIVATE);
+        int mySavedNumber = sharedPreferences.getInt("myNumber",0);
+        mySavedNumber = mySavedNumber + myNumber;
+        System.out.println(mySavedNumber);
+        sharedPreferences.edit().putInt("myNumber",mySavedNumber).apply();
+    }
+}
